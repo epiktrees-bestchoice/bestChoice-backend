8e137b1ba8ee4f34a72294cf8c61392ff9192c8f
sumin-Heo <sumin3985@gmail.com>
2023-09-04 17:03:18 +0900

    그룹 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	kotlin("plugin.jpa") version "1.8.22"
 }
 
-group = "com.example"
+group = "bestChoice-backend"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
