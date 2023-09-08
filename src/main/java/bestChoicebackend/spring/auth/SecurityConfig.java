d63578d04a39d7a0be6ca92f913c9066bd8115d4
Sol <haxr369@gmail.com>
2023-09-08 11:26:03 +0900

    SecurityConfig create

---
+++ src/main/java/bestChoicebackend/spring/auth/SecurityConfig.java
@@ -0,0 +1,32 @@
+package bestChoicebackend.spring.auth;
+
+import bestChoicebackend.spring.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig{
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/","css/**","/images/**","/js/**","/h2-console/**","/profile").permitAll()
+                        .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated())
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/")  )
+                .oauth2Login((oauth2Login) -> oauth2Login
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService));
+    }
+}
