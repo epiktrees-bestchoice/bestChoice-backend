1e3b65b4d9103049d7c221bd0598f50096357a1a
Sol <haxr369@gmail.com>
2023-09-08 10:52:32 +0900

    role enum

---
+++ src/main/java/bestChoicebackend/spring/domain/Role.java
@@ -0,0 +1,16 @@
+package bestChoicebackend.spring.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
