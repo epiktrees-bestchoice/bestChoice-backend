2dc1fec8dca840d039ecf842f7bd5c70727f0818
ohchansol <haxr369@gmail.com>
2023-09-20 15:55:56 +0900

    fix 유저정보 업데이트 service and Feat userController

--- src/main/java/bestChoicebackend/spring/config/auth/dto/OAuthAttributes.java
+++ src/main/java/bestChoicebackend/spring/config/auth/dto/OAuthAttributes.java
@@ -106,7 +106,7 @@ public class OAuthAttributes {
                 .userEmail(userEmail)
                 .social(social)
                 .picture(picture)
-                .role(Role.GUEST)	// 가입 기본 권한 == GUEST
+                .role(Role.USER)	// 가입 기본 권한 == USER
                 .build();
     }
 }
--- src/main/java/bestChoicebackend/spring/controller/UserController.java
+++ src/main/java/bestChoicebackend/spring/controller/UserController.java
@@ -1,10 +1,19 @@
 package bestChoicebackend.spring.controller;
 
+import bestChoicebackend.spring.config.auth.dto.SessionUser;
+import bestChoicebackend.spring.domain.User;
+import bestChoicebackend.spring.dto.UserInfoRequestDto;
 import bestChoicebackend.spring.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/api/v1/my")
 public class UserController {
 
     private final UserService userService;
@@ -15,5 +24,32 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/info")
+    @ResponseBody
+    public Optional<User> getUserInfo(HttpSession httpSession){
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        return userService.findByUserEmail(sessionUser.getEmail());
+    }
+
+    @GetMapping("/manage")
+    public String putForm() {
+        return "putUserForm";
+    }
+
+    @PostMapping("/info")
+    @ResponseBody
+    public User putUserInfo(HttpSession httpSession, UserInfoRequestDto userInfoRequestDto){
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        User user = userService.findByUserEmail(sessionUser.getEmail())
+                .orElseThrow();
+
+        userService.update(user,
+                userInfoRequestDto.getName(),
+                userInfoRequestDto.getNickName(),
+                userInfoRequestDto.getPhoneNumber(),
+                userInfoRequestDto.getPicture());
+
+        return user;
+    }
 
 }
