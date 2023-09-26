79a5cd58356b15f6c5f26e27bc422e6508d00e31
ohchansol <haxr369@gmail.com>
2023-09-26 09:48:30 +0900

    add context-path

--- src/main/java/bestChoicebackend/spring/config/auth/SecurityConfig.java
+++ src/main/java/bestChoicebackend/spring/config/auth/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
