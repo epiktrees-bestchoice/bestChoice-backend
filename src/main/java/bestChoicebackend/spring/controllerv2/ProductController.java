335cb608fe2f5e004a306139eb7a6f2c6c05aa13
ohchansol <haxr369@gmail.com>
2023-11-07 17:45:25 +0900

    fix controller

--- src/main/java/bestChoicebackend/spring/controllerv2/ProductController.java
+++ src/main/java/bestChoicebackend/spring/controllerv2/ProductController.java
@@ -8,6 +8,7 @@ import bestChoicebackend.spring.service.SubImgService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,6 +16,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,18 +30,28 @@ public class ProductController {
 
     @GetMapping(value = "/search/{type}")
     public Page<AccommodationResDto> getReq(@PathVariable("type") String type,
-                                            //SearchReqDto searchReqDto, // Controller에서 받는 json을 DTO로 변환, NotNull 판단,.
                                             @RequestParam String region, @RequestParam LocalDate sel_date, @RequestParam LocalDate sel_date2,
                                             @RequestParam Integer min_price, @RequestParam Integer max_price, @RequestParam List<Integer> keywords,
                                             @PageableDefault(size=10, direction = Sort.Direction.DESC) Pageable pageable) {
-        SearchReqDto searchReqDto = SearchReqDto.builder()
-                .region(region)
-                .sel_date(sel_date)
-                .sel_date2(sel_date2)
-                .max_price(max_price)
-                .min_price(min_price)
-                .keywords(keywords)
-                .build();
+        SearchReqDto.SearchReqDtoBuilder searchReqDtoBuilder = SearchReqDto.builder();
+        if(region == null) searchReqDtoBuilder.region("서울");
+        else searchReqDtoBuilder.region(region);
+
+        if(sel_date == null) searchReqDtoBuilder.sel_date(LocalDate.now());
+        else searchReqDtoBuilder.sel_date(sel_date);
+
+        if(sel_date2 == null) searchReqDtoBuilder.sel_date(LocalDate.now());
+        else searchReqDtoBuilder.sel_date2(sel_date2);
+
+        if(min_price == null) searchReqDtoBuilder.min_price(0);
+        else searchReqDtoBuilder.min_price(min_price);
+
+        if(max_price == null) searchReqDtoBuilder.max_price(10000000);
+        else searchReqDtoBuilder.max_price(max_price);
+
+        if(keywords == null) searchReqDtoBuilder.keywords(new ArrayList<Integer>());
+        else searchReqDtoBuilder.keywords(keywords);
+        SearchReqDto searchReqDto = searchReqDtoBuilder.build();
 
         log.info("type : "+type+" region : "+searchReqDto.getRegion()+" SelDate : "+searchReqDto.getSel_date()+" maxPrice : " + searchReqDto.getMax_price());
         return productService.getProductWithCondition(type, searchReqDto, pageable);
